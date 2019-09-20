9e8048ca0400862430113ff6e4d61ed58e98a21d
Alec <a_riddick@fanshaweonline.ca>
2019-09-20 19:09:26 -0400

    Added Bio Image to dataObject

--- js/modules/dataObject.js
+++ js/modules/dataObject.js
@@ -8,4 +8,12 @@ export default {
     casual: "assets/aboutCasualNour.jpg"
   },
 
+  Alec: {
+    computer: "Mac or PC: PC Master Race. Bow before me",
+    pet: "Any Pets: A dog named Ajaxx, and two and a half horses; Lincoln, Nellie, and Gracie",
+    drink: "Fav Drink: Water",
+    food: "Fav Food: Chicken Burritos",
+    quote: "Fav Quote: If you have purpose, you don't need motivation. Purpose gets you through all the bullshit that people recycle as excuses. Either you're getting shit done or you aren't. - Papa Swolio",
+    casual: "images/aboutCasualAlec.jpg"
+  },
 }
