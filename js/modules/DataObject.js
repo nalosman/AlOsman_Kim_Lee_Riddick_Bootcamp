c77d90f0cc6127b28c566f8ad1334c9967da4995
nour <nour@nours-MBP.fc.ca>
2019-09-20 15:14:27 -0400

    coded the work page and started the DataObject JS file

---
+++ js/modules/DataObject.js
@@ -0,0 +1,9 @@
+export default {
+  Nour: {
+    computer: "Mac or PC: Mac all the way!",
+    drink: "Fav Drink: Coffee all day everyday",
+    food: "Fav Food: Fettucine Alfredo",
+    pet: "Pet: A cat named Peaches",
+    quote: "Fav Desiqn Quote: You can't use up creativity. The more you use, the more you have -- Maya Angelou, author, poet, civil rights activist"
+  },
+}
