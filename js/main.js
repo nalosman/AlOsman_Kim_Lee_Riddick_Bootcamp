4950155196f5d1f32694a65a12c474fee28c1824
Stella <feelis1119@naver.com>
2019-09-20 14:38:24 -0400

    final-home/contact

--- js/main.js
+++ js/main.js
@@ -2,13 +2,24 @@
 	//this is a self invoking function
 	console.log('My JS issa workin!');
 	
-	var button = document.querySelector("#button");
-	var burgerCon = document.querySelector("#burgerCon");
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burgerCon");
+
+	let viewMore = document.querySelector("#viewMore");
 
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slideToggle");
 		button.classList.toggle("expanded");
 	}
 
+	function linkToWorkPage() {
+		window.location.href = 'work.html';
+	}
+
+
 	button.addEventListener("click", hamburgerMenu, false);
+	viewMore.addEventListener("click", linkToWorkPage);
+
+
+	
 })();
