85c497519feafc3917d624f19b85b7fe94a7fe23
Stella <feelis1119@naver.com>
2019-09-20 23:03:23 -0400

    about/finalv

--- js/main.js
+++ js/main.js
@@ -1,25 +1,83 @@
+import Team from './modules/dataObject.js'; 
+	
 (function(){
 	//this is a self invoking function
 	console.log('My JS issa workin!');
 	
-	let button = document.querySelector("#button");
-	let burgerCon = document.querySelector("#burgerCon");
+	let button = document.querySelector('#button');
+	let burgerCon = document.querySelector('#burgerCon');
 
-	let viewMore = document.querySelector("#viewMore");
+	// let viewMore = document.getElementById('viewMore');
+
+	let bioCon = document.querySelector('.bioCon').children;
+
+	let playBut = document.querySelector('.playBut'),
+		pauseBut = document.querySelector('.pauseBut'),
+		rewindBut = document.querySelector('.rewindBut'),
+		promoVid = document.querySelector('.promoVid');
+
+	
+	function pauseVideo() {
+		promoVid.pause();
+	}
+
+	function playVideo() {
+		promoVid.play();
+	}
+
+	function rewindVideo() {
+		promoVid.currentTime = 0;
+	}
+
 
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slideToggle");
 		button.classList.toggle("expanded");
 	}
 
-	function linkToWorkPage() {
-		window.location.href = 'work.html';
+	// function linkToWorkPage() {
+	// 	window.location.href = 'work.html';
+	// }
+
+
+	function showMemberBio() {
+		//this = refers to element that call to function
+		let person = this.textContent;
+		
+		bioCon[0].src = `assets/${Team[person].casual}`;
+		bioCon[1].textContent = Team[person].computer;
+		bioCon[2].textContent = Team[person].pet;
+		bioCon[3].textContent = Team[person].drink;
+		bioCon[4].textContent = Team[person].food;
+		bioCon[5].textContent = Team[person].quote;
+	}	
+
+	for (let person in Team) {
+		console.log(person);
+
+		let memberButton = document.createElement('button');
+		memberButton.classList.add("memberBut");
+
+		memberButton.textContent = person;
+		memberButton.addEventListener("click", showMemberBio);
+
+		document.querySelector('.team').appendChild(memberButton);
+		
 	}
 
 
-	button.addEventListener("click", hamburgerMenu, false);
-	viewMore.addEventListener("click", linkToWorkPage);
 
+
+	
+
+	
+	playBut.addEventListener("click", playVideo);
+	pauseBut.addEventListener("click", pauseVideo);
+	rewindBut.addEventListener("click", rewindVideo);
+
+	button.addEventListener("click", hamburgerMenu, false);
+	// viewMore.addEventListener("click", linkToWorkPage);
+	
 
 	
 })();
---
+++ js/modules/dataObject.js
@@ -0,0 +1,37 @@
+export default {
+  Nour: {
+    computer: "Mac or PC: Mac all the way!!!",
+    pet: "Any Pets: Cat named Peaches",
+    drink: "Fav Drink: Coffee all day, everyday!",
+    food: "Fav Food: Fettucine Alfredo",
+    quote: "Fav Quote: You can’t use up creativity. The more you use, the more you have. - Maya Angelou",
+    casual: "aboutCasualNour.jpg"
+  },
+
+  Alec: {
+    computer: "Mac or PC: PC Master Race. Bow before me",
+    pet: "Any Pets: A dog named Ajaxx, and two and a half horses; Lincoln, Nellie, and Gracie",
+    drink: "Fav Drink: Water",
+    food: "Fav Food: Chicken Burritos",
+    quote: "Fav Quote: If you have purpose, you don't need motivation. Purpose gets you through all the bullshit that people recycle as excuses. Either you're getting shit done or you aren't. - Papa Swolio",
+    casual: "aboutCasualAlec.jpg"
+  },
+
+  Hyeryn: {
+    computer: "Mac or PC: PC PC PC... PC will save the world",
+    pet: "Any Pets: 2 cats and a dog",
+    drink: "Fav Drink: Diet Coke",
+    food: "Fav Food: Korean Style Chicken",
+    quote: "Fav Quote: Think like a man of action and act like man of thought.",
+    casual: "aboutCasualHyeryn.jpg"
+  },
+
+  Gahyun: {
+    computer: "Mac or PC: Mac!! Who uses PC?",
+    pet: "Any Pets: 2 cats",
+    drink: "Fav Drink: Canada Dry",
+    food: "Fav Food: Intestine",
+    quote: "Fav Quote: Rescue Yourself!",
+    casual: "aboutCasualGahyun.jpg"
+  },
+}
